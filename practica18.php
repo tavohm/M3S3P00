51c4e6dc3a7ce8ad8cd73646d524f35ec32a0350
tavohm <gustavo.hernandez.cbtis94@gmail.com>
2020-03-27 11:56:07 -0600

    actualizado con sesion del 27 de marzo.

--- practica18.php
+++ practica18.php
@@ -1,13 +1,17 @@
  <link href="style.css" rel="stylesheet" type="text/css">
  <?php
 
+   // instanciamos un objeto    
     $ecuacion = new Ecuacion();
 
+    // pasamos los valores de las variables a las variables de la clase
     $ecuacion->setA($_POST["a"]);
     $ecuacion->setB($_POST["b"]);
     $ecuacion->setC($_POST["c"]);
 
+    // usamos el método Soluciones para resolver la ecucación
     $ecuacion->Soluciones();
+
 
     class Ecuacion{
         // atributos
@@ -17,6 +21,9 @@
 
         // constructor
         public function __construct(){ }
+
+        // destructor de la clase
+        public function __destruct() { } 
 
         // getters
         public function getA(){
@@ -42,19 +49,23 @@
 
         // Discriminante, si D<0 no hay soluciones, si D == 0 hay una solución y si D>0 hay dos soluciones.
         public function Discriminante(){
-            $D = $this->getB()**2 - 4 * $this->getA() * $this->getC();
+            $D = $this->getB()**2 - 4 * $this->getA() * $this->getC(); // b^2 - 4ac
             return $D;
         }
 
         public function Soluciones(){
-            if ( $this->Discriminante() < 0) {
+            if ( $this->Discriminante() < 0) { // si D<0 no se puede encontrar una raiz negativa, no hay respuestas
                 echo "No existen soluciones!";
-            }else if( $this->Discriminante() == 0){
-                echo "X = ". (-$this->getB() + sqrt($this->Discriminante()) / (2 * $this->getA()));
-            }else{
+            }else if( $this->Discriminante() == 0){ // si D==0 las dos respuestas son iguales, solo existe una solución
+                // x = -b +- sqrt(b^2-4*a*c)/2*a
+                echo "X1 = ". (-$this->getB() + sqrt($this->Discriminante()) / (2 * $this->getA()))."<br/>";
+                echo "X2 = ". (-$this->getB() - sqrt($this->Discriminante()) / (2 * $this->getA()));
+            }else{ // si D>0 existen 2 soluciones
                 echo "X1 = ". (-$this->getB() + sqrt($this->Discriminante()) / (2 * $this->getA()))."<br/><br/>";
                 echo "X2 = ". (-$this->getB() - sqrt($this->Discriminante()) / (2 * $this->getA()));
             }
         }
     }
+
+ 
  ?>
